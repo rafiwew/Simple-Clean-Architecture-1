526f68ca8cadb109cd1dad6f3e988e06dba526f5
Muhammad Rafi <mochrafi01@gmail.com>
2023-12-25 08:42:14 +0700

    added testing use case 😈

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,11 +39,17 @@ android {
 }
 
 dependencies {
-
+    // Kotlin lang
     implementation("androidx.core:core-ktx:1.12.0")
+
+    // UI things
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Testing
+    testImplementation("org.mockito:mockito-core:5.2.0")
+    testImplementation("org.mockito:mockito-inline:5.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
